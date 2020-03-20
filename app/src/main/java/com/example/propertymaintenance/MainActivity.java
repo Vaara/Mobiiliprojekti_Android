be6816857578408e02ee1daa634bdd44426484e4
Mulveni <t7koii00@students.oamk.fi>
2020-03-20 09:08:09 +0200

    Nakyman raakaversion muokkaus

--- app/src/main/java/com/example/propertymaintenance/MainActivity.java
+++ app/src/main/java/com/example/propertymaintenance/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.propertymaintenance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.buttonActivity).setOnClickListener(this);
+
+    }
+    @Override
+    public void onClick(View v) {
+    launchActivity();
+    }
+    private void launchActivity() {
+
+        Intent intent = new Intent(this, ServiceAdvices.class);
+        startActivity(intent);
     }
 }
