96d3c2affe4442ce12d1b731c1ac20bfbb385367
t8letu01 <t8letu01@students.oamk.fi>
2020-03-22 10:18:50 +0200

    Created network_security_config.xml to allow cleartext

--- app/src/main/java/com/example/propertymaintenance/LoginActivity.java
+++ app/src/main/java/com/example/propertymaintenance/LoginActivity.java
@@ -12,17 +12,21 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -66,17 +70,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     @Override
     public void onClick(View view) {
-        checkLoginCredentials();
+        if (view.getId() == R.id.btnLogin) {
+            checkLoginCredentials();
+        }
     }
 
     public void checkLoginCredentials() {
-
         final ProgressDialog progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Logging in...");
         progressDialog.show();
 
         RequestQueue queue = Volley.newRequestQueue(this);
         final String url = "http://ec2-18-234-159-189.compute-1.amazonaws.com/login";
+        //?datatype=json
 
         JSONObject loginUser = new JSONObject();
         try {
@@ -88,19 +94,22 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             e.printStackTrace();
         }
 
-        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, loginUser,
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, url, loginUser ,
                 new Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
                         progressDialog.dismiss();
                         try {
                             userIdResponse = response.getInt("userId");
-                            userLevelResponse = response.optInt("userLevel");
-                            userFullNameResponse = response.optString("userFullName");
-                            userHousingCooperativeIdResponse = response.optInt("userHousingCooperativeId");
-                            userPropertyMaintenanceIdResponse = response.optInt("userPropertyMaintenanceId");
+                            //userLevelResponse = response.optInt("userLevel");
+                            //userFullNameResponse = response.optString("userFullName");
+                            //userHousingCooperativeIdResponse = response.optInt("userHousingCooperativeId");
+                            //userPropertyMaintenanceIdResponse = response.optInt("userPropertyMaintenanceId");
 
-                            storeUserToSharedPrefs();
+                            //storeUserToSharedPrefs();
+                            //login();
 
                         } catch (JSONException e) {
                             e.printStackTrace();
@@ -110,11 +119,106 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG).show();
+                        //Toast.makeText(LoginActivity.this, "Login failed", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
                     }
                 }
         );
         queue.add(getRequest);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+        /*
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+                        try {
+                            JSONObject obj = new JSONObject(response);
+
+                            if (!obj.getBoolean("error")) {
+                                Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_LONG).show();
+
+                                userIdResponse = obj.getInt("userId");
+                                userLevelResponse = obj.optInt("userLevel");
+                                userFullNameResponse = obj.optString("userFullName");
+                                userHousingCooperativeIdResponse = obj.optInt("userHousingCooperativeId");
+                                userPropertyMaintenanceIdResponse = obj.optInt("userPropertyMaintenanceId");
+
+                                storeUserToSharedPrefs();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                })
+        {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", "test1");
+                params.put("password", "123");
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+         */
+
+    }
+
+    public void getUserFromSharedPrefs() {
+        Integer userId = sharedPreferences.getInt(USER_ID, 99);
+        Integer userLevel = sharedPreferences.getInt(USER_LEVEL, 99);
+        String userFullName = sharedPreferences.getString(USER_FULL_NAME, "");
+        Integer userHousingCooperativeId;
+        Integer userPropertyMaintenanceId;
+
+        if (userId == 0) {
+            userHousingCooperativeId = sharedPreferences.getInt(USER_HOUSING_COOPERATIVE_ID, 99);
+        }
+
+        else {
+            userPropertyMaintenanceId = sharedPreferences.getInt(USER_PROPERTY_MAINTENANCE_ID, 99);
+        }
+    }
+
+    public Integer getUserIdFromSharedPrefs() {
+        Integer userId = sharedPreferences.getInt(USER_ID, 99);
+        return userId;
+    }
+
+    public Integer getUserLevelFromSharedPrefs() {
+        Integer userLevel = sharedPreferences.getInt(USER_LEVEL, 99);
+        return userLevel;
+    }
+
+    public String getUserFullNameFromSharedPrefs() {
+        String userFullName = sharedPreferences.getString(USER_FULL_NAME, "");
+        return userFullName;
+    }
+
+    public Integer getUserHousingCooperativeIdFromSharedPrefs() {
+        Integer userHousingCooperativeId = sharedPreferences.getInt(USER_HOUSING_COOPERATIVE_ID, 99);
+        return userHousingCooperativeId;
+
+    }
+
+    public Integer getUserPropertyMaintenanceIDFromSharedPrefs() {
+        Integer userPropertyMaintenanceId = sharedPreferences.getInt(USER_PROPERTY_MAINTENANCE_ID, 99);
+        return userPropertyMaintenanceId;
     }
 
     public void storeUserToSharedPrefs() {
@@ -169,6 +273,5 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     public void logout() {
         removeUserFromSharedPrefs();
-        // Show LoginScreen ?
     }
 }
