5d2c796830dac561638d21908a8e2c15cf0016df
Mulveni <t7koii00@students.oamk.fi>
2020-03-23 06:44:30 +0200

    muutoksia nakymaan

--- app/src/main/java/com/example/propertymaintenance/ServiceAdvices.java
+++ app/src/main/java/com/example/propertymaintenance/ServiceAdvices.java
@@ -1,14 +1,85 @@
 package com.example.propertymaintenance;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class ServiceAdvices extends AppCompatActivity {
+public class ServiceAdvices extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int PERMISSION_CODE = 1000;
+    private int IMAGE_CAPTURE_CODE = 1001;
+    private static final int PICK_IMAGE = 1002;
+    ImageView imageView;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_service_advices);
+        findViewById(R.id.imageButtonCamera).setOnClickListener(this);
     }
-}
+
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.imageButtonCamera) {
+            Intent intent= new Intent(MediaStore.INTENT_ACTION_STILL_IMAGE_CAMERA);
+            startActivity(intent);
+            if(v.getId()== R.id.attachment){
+                openGallery();
+            }
+            //openCamera();
+        }
+    }
+
+    private void openGallery() {
+        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            //imageUri = data.getData();
+            //imageView.setImageURI(imageUri);
+        }
+    }
+
+
+/*    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+        Uri imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE);
+    }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_CODE:{
+                if (grantResults.length > 0 && grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                    openCamera();
+                } else {
+                    Toast.makeText(this, "Permissiondenied...", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK){
+            mImageView.setImageURI(imageUri);
+
+ }
+ }
+ */       }
