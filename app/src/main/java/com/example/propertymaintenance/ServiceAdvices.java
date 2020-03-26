fab0dc0ed2ad5bfd1894177ccace093421addc96
Mulveni <t7koii00@students.oamk.fi>
2020-03-26 10:50:50 +0200

    added windowmanager to keep buttons down even when keyboard is up

--- app/src/main/java/com/example/propertymaintenance/ServiceAdvices.java
+++ app/src/main/java/com/example/propertymaintenance/ServiceAdvices.java
@@ -12,6 +12,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ public class ServiceAdvices extends BaseActivity implements View.OnClickListener
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
     }
 
